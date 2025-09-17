fd770ad6bb1c91f1ffde8c66fa30f5b44ace9eda
Lucas Vandermaarel <Lvande10@my.centennialcollege.ca>
2025-09-17 15:30:32 -0400

    working on prettying the navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <br />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -1,28 +1,37 @@
-
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import homeImg from "../assets/home.png";
 
 export default function NavBar() {
-    return (
-        <>
-        <nav className="">
-            <ul className="flex space-x-4 p-4">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About Me</Link>
-                </li>
-                <li>
-                    <Link to="/projects">Projects</Link>
-                </li>
-                <li>
-                    <Link to="/services">Services</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
-        </>
-    );
+  const location = useLocation();
+
+  const navbarLocations = [
+    { to: "/", label: <img
+                className="mx-auto lg:mx-4"
+                width="25"
+                height="25"
+                src={homeImg}
+                alt="Lucas Vandermaarel Logo"
+              /> },
+    { to: "/about", label: "About Me" },
+    { to: "/projects", label: "Projects" },
+    { to: "/services", label: "Services" },
+    { to: "/contact", label: "Contact" }
+  ]
+
+  return (
+    <>
+      <nav className="navbar rounded-2xl justify-items-center">
+        <ul className="flex w-full md:w-auto flex-col md:flex-row lg:space-x-4 lg:space-y-0 space-y-2 rounded-4xl bg-white/10">
+          {navbarLocations.map(({ to, label }) => (
+            <li
+              key={to}
+              className={`px-2 ${location.pathname === to ? "bg-white/20" : ""} transition-color duration-200`}
+            >
+              <Link to={to}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
 }
