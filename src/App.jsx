0742f4667b549dd746c142062908908d63e5ea8b
Lucas <lukevdm64@gmail.com>
2025-09-24 20:48:57 -0400

    fixing casing issue in naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/navbar.jsx";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Contact from "./pages/contact.jsx";
-import Services from "./pages/services.jsx";
+import NavBar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
+import Services from "./pages/Services.jsx";
 import "./App.css";
 
 function App() {
