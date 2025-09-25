e7c79b399143a19a6aab6088fbd3d1a3b3ea76e9
Lucas <lukevdm64@gmail.com>
2025-09-24 21:02:21 -0400

    home.jsx isn't being found in netlify.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/Navbar.jsx";
-import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Projects from "./pages/Projects.jsx";
 import Contact from "./pages/Contact.jsx";
 import Services from "./pages/Services.jsx";
+import Home from "./pages/Home.jsx";
 import "./App.css";
 
 function App() {
